660d4455e52e4a82b8d98a6aa8c6815314d1edea
Aravind-Coderr <87963631+Aravind-Coderr@users.noreply.github.com>
2022-02-24 01:45:09 +0530

    Update MygroovyScript.groovy

--- MygroovyScript.groovy
+++ MygroovyScript.groovy
@@ -3,17 +3,17 @@ pipeline{
 	stages{
 			stage('Fuck you'){
 				steps{
-				sh echo "Fuck you Bitch"
+				sh 'echo "Fuck you Bitch"'
 				}
 			}
 			stage('Bitch'){
 				steps{
-				sh echo "Fuck you Bitch"
+				sh 'echo "Fuck you Bitch"'
 				}
 			}
 			stage('deploy'){
 				steps{
-				sh echo "Fuck you Bitch"
+				sh 'echo "Fuck you Bitch"'
 				}
 			}
 		}
