d9aea47da38d90dbccf417dd984c26a78258441b
Aravind-Coderr <87963631+Aravind-Coderr@users.noreply.github.com>
2022-02-24 00:29:30 +0530

    Add files via upload

---
+++ MygroovyScript.groovy
@@ -0,0 +1,20 @@
+pipeline{
+	agent any
+	stages{
+			stage('Fuck you'){
+				steps{
+				sh echo "Fuck you Bitch"
+				}
+			}
+			stage('Bitch'){
+				steps{
+				sh echo "Fuck you Bitch"
+				}
+			}
+			stage('deploy'){
+				steps{
+				sh echo "Fuck you Bitch"
+				}
+			}
+		}
+}
