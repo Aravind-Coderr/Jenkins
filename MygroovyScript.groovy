130cab038880fd74db645baf336e91201c81eccf
Aravind-Coderr <87963631+Aravind-Coderr@users.noreply.github.com>
2022-02-24 01:52:22 +0530

    Update MygroovyScript.groovy

--- MygroovyScript.groovy
+++ MygroovyScript.groovy
@@ -3,16 +3,18 @@ pipeline{
 	stages{
 			stage('Fuck you'){
 				steps{
-				
+				sh "echo Hello World"
 				}
 			}
 			stage('Bitch'){
 				steps{
+					sh "echo Hello World"
 				
 				}
 			}
 			stage('deploy'){
 				steps{
+					sh "echo Hello World"
 				
 				}
 			}
