045548feb116d2290d819a966fea6182554a37f3
Aravind-Coderr <87963631+Aravind-Coderr@users.noreply.github.com>
2022-02-24 12:12:50 +0530

    Update MygroovyScript.groovy

--- MygroovyScript.groovy
+++ MygroovyScript.groovy
@@ -1,9 +1,17 @@
 pipeline{
 	agent any
 	stages{
+		environment { 
+        		Name = 'Aravind you are a RockStar'
+    		}
 			stage('Fuck you'){
 				steps{
-				echo "Hello World"
+				echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
+				echo "Running ${env.BUILD_NUMBER} on ${env.JOB_NAME}"
+				echo "Running ${env.WORKSPACE} on ${env.JENKINS_URL}"
+				echo "${Name}"
+
+				
 				}
 			}
 			stage('Bitch'){
